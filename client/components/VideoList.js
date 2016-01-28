f79318d045649d651968d831515b369ba41886e4
jwdomingo <johnwill.domingo@gmail.com>
2016-01-27 21:22:57 -0800

    Add key to each video list entry item

--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -2,8 +2,8 @@ class VideoList extends React.Component {
 
   render(){
     return (<div className="video-list media">
-    {this.props.playlist.map(videoEntry => 
-    <VideoListEntry video={this.props.video} videoEntry={videoEntry} listener={this.props.listener}/>
+    {this.props.playlist.map((videoEntry, key) => 
+    <VideoListEntry video={this.props.video} videoEntry={videoEntry} listener={this.props.listener} key={key}/>
     )}
     </div>);
   }
