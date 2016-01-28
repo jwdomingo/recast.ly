222dbb39ba10ea1d2dfe544ba737a2582b63595d
jwdomingo <johnwill.domingo@gmail.com>
2016-01-27 16:58:31 -0800

    Refactor to classical instantiation

--- client/components/VideoListEntry.js
+++ client/components/VideoListEntry.js
@@ -1,15 +1,3 @@
-// var VideoListEntry = () => (
-//   <div className="video-list-entry">
-//     <div className="media-left media-middle">
-//       <img className="media-object" src="//i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg" alt="" />
-//     </div>
-//     <div className="media-body">
-//       <div className="video-list-entry-title">Video Title</div>
-//       <div className="video-list-entry-detail">Video Description</div>
-//     </div>
-//   </div>
-// );
-
 class VideoListEntry extends React.Component {
   constructor(props) {
     super(props);
--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -1,5 +1,9 @@
-var VideoPlayer = () => (
-  <div className="video-player">
+class VideoPlayer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (<div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
       <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
     </div>
@@ -7,7 +11,8 @@ var VideoPlayer = () => (
       <h3>Video Title</h3>
       <div>Video Description</div>
     </div>
-  </div>
-);
+  </div>);
+  }
+}
 
 window.VideoPlayer = VideoPlayer;
