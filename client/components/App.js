0b41603653adfc0ed76630aca4cbb8fa252a793a
jwdomingo <johnwill.domingo@gmail.com>
2016-01-27 16:56:27 -0800

    Reimplement App class

--- client/components/App.js
+++ client/components/App.js
@@ -1,14 +1,21 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {done: true};
+  }
+
+  render(){
+    return (<div>
+      <Nav/>
+      <div className="col-md-7">
+        <VideoPlayer/>
+      </div>
+      <div className="col-md-5">
+        <VideoList/>
+      </div>
+    </div>);
+  };
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
