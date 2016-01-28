ddcee861a471580c472afa87bcf50269df4b46e4
jwdomingo <johnwill.domingo@gmail.com>
2016-01-27 16:57:31 -0800

    Refactor to classical instantiation

--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -1,9 +1,15 @@
-var VideoList = () => (
-  <div className="video-list media">
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    console.log('HELLOOOO', this.props.anything)
+    return (<div className="video-list media">
     {window.exampleVideoData.map(video => 
-        <VideoListEntry vdata={video}/>
+    <VideoListEntry vdata={video}/>
     )}  
-  </div>
-);
+    </div>);
+  }
+}
 
 window.VideoList = VideoList;
