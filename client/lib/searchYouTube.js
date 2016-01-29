8d1b1b26a168a2fd89897c0373c990c9e5dd401d
jwdomingo <johnwill.domingo@gmail.com>
2016-01-28 16:08:01 -0800

    Refactor success logic to invoke callback with data

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,10 +1,14 @@
 var searchYouTube = (options, callback) => {
-  $.ajax({
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    method: 'GET',
-    success(data) => {
+  return $.ajax({
+    url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${options.query}&key=${window.YOUTUBE_API_KEY}`,
+    type: 'GET',
+    contentType: 'application/json',
+    success (data) {
+      if(data.items || data.items.length){
+        callback(data.items);
+      }
     },
-    error(data) => {
+    error (data) {
       console.error('Something bad happened.');
     }
   });
