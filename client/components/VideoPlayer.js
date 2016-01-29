52494882f58906702b9ae2947d4c4c5de49e2010
jwdomingo <johnwill.domingo@gmail.com>
2016-01-28 16:34:08 -0800

    Add default logic using ternary operators

--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -3,11 +3,17 @@ class VideoPlayer extends React.Component {
 
     return (<div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${this.props.video.id.videoId}?autoplay=1`} allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={
+        !!this.props.video ? `https://www.youtube.com/embed/${this.props.video.id.videoId}?autoplay=1` : `http://4.bp.blogspot.com/-dGQzAh5mojE/TfUpQPXo2-I/AAAAAAAAAI8/586KSofMt84/s1600/Untitled-1+copy.jpg`
+      } allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>{this.props.video.snippet.title}</h3>
-      <div>{this.props.video.snippet.description}</div>
+      <h3>{
+        !!this.props.video ? this.props.video.snippet.title : 'Video not found'
+      }</h3>
+      <div>{
+        !!this.props.video ? this.props.video.snippet.description : ''
+      }</div>
     </div>
   </div>);
   }
