5f1e09c9c057e6d23e6d43cf08290ada09391715
jwdomingo <johnwill.domingo@gmail.com>
2016-01-28 16:14:04 -0800

    Refactor onChange handler

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,10 +1,24 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+class Search extends React.Component{
+
+  render(){
+    return <div className="search-bar form-inline">
+      <input className="form-control" type="text" onChange={(event) => {
+        var options = {
+          query: event.target.value,
+          max: 5,
+          key: window.YOUTUBE_API_KEY
+        };
+
+        searchYouTube(options,this.props.callback)}
+        }
+      />
+      <button className="btn hidden-sm-down">
+        <span className="glyphicon glyphicon-search"></span>
+      </button>
+    </div> 
+  }
+}
+
 
 window.Search = Search;
+
